a277d7eeff387a8d06ef8132e5341250d29ddd59
gbzhu <gbzhu@zju.edu.cn>
2018-12-05 16:40:57 +0800

    add logon

--- app/src/main/java/com/example/gbzhu/firstapp/MainActivity.java
+++ app/src/main/java/com/example/gbzhu/firstapp/MainActivity.java
@@ -12,12 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button mBtnTextView = findViewById(R.id.btn_textview);
+        Button mBtnLogon = findViewById(R.id.btn_logon);
+
+
         mBtnTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 跳转到textview的演示界面
                 Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mBtnLogon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // jump to logon page
+                Intent intent = new Intent(MainActivity.this, LogonActivity.class);
                 startActivity(intent);
             }
         });
